e26f2c26dd96aab85a37ca1b587f5d6de9445f3b
Bohdan Shcherbak <b.szczerbak@gmail.com>
2024-10-29 21:13:24 +0100

    Remove swagger from companies controller

--- src/modules/companies/companies.controller.ts
+++ src/modules/companies/companies.controller.ts
@@ -1,14 +1,11 @@
 import { Body, Controller, Delete, Get, Param, ParseUUIDPipe, Post, Put } from '@nestjs/common';
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { CompaniesService } from './companies.service';
 import { CreateCompanyDto } from './dtos/create-company.dto';
 import { UpdateCompanyDto } from './dtos/update-company.dto';
 import { AuthUser } from '../auth/user.decorator';
 import { AuthUserDto } from '../auth/dtos/auth.user.dto';
-import { CompanyDto } from './dtos/company.dto';
 import { CompanyMapper } from './companies.mapper';
 
-@ApiTags('companies')
 @Controller('companies')
 export class CompaniesController {
   constructor(
@@ -17,60 +14,18 @@ export class CompaniesController {
   ) {}
 
   @Post()
-  @ApiOperation({ summary: 'Create company' })
-  @ApiResponse({
-    status: 201,
-    description: 'Company created',
-    type: CompanyDto,
-  })
-  @ApiResponse({
-    status: 400,
-    description: 'Validation error',
-  })
-  @ApiResponse({
-    status: 404,
-    description: 'JobApplication not found',
-  })
   async createCompany(@Body() createCompanyDto: CreateCompanyDto, @AuthUser() user: AuthUserDto) {
     const company = await this.companiesService.create(createCompanyDto, user);
     return this.mapper.toDto(company);
   }
 
   @Get('/:id')
-  @ApiOperation({ summary: 'Fetch company.' })
-  @ApiResponse({
-    status: 200,
-    description: 'Company record',
-    type: CompanyDto,
-  })
-  @ApiResponse({
-    status: 400,
-    description: 'Validation error',
-  })
-  @ApiResponse({
-    status: 404,
-    description: 'Company not found',
-  })
   async getCompany(@Param('id', ParseUUIDPipe) companyId: string, @AuthUser() user: AuthUserDto) {
     const company = await this.companiesService.findOne(companyId, user);
     return this.mapper.toDto(company);
   }
 
   @Put('/:id')
-  @ApiOperation({ summary: 'Updates company' })
-  @ApiResponse({
-    status: 201,
-    description: 'Company updated',
-    type: CompanyDto,
-  })
-  @ApiResponse({
-    status: 400,
-    description: 'Validation error',
-  })
-  @ApiResponse({
-    status: 404,
-    description: 'Company or JobApplication not found',
-  })
   async updateCompany(
     @Param('id', ParseUUIDPipe) companyId: string,
     @Body() updateCompanyDto: UpdateCompanyDto,
@@ -81,19 +36,6 @@ export class CompaniesController {
   }
 
   @Delete('/:id')
-  @ApiOperation({ summary: 'Delete company' })
-  @ApiResponse({
-    status: 201,
-    description: 'Company deleted',
-  })
-  @ApiResponse({
-    status: 400,
-    description: 'Validation error',
-  })
-  @ApiResponse({
-    status: 404,
-    description: 'Company or JobApplication not found',
-  })
   deleteCompany(@Param('id', ParseUUIDPipe) companyId, @AuthUser() user: AuthUserDto) {
     return this.companiesService.remove(companyId, user);
   }
