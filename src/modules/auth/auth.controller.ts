1a7a0262185eb86f182df437490f39a16a033cd1
Bohdan Shcherbak <b.szczerbak@gmail.com>
2024-10-29 21:10:43 +0100

    Remove swagger from auth controller

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -9,14 +9,11 @@ import {
   UnauthorizedException,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { SignInDto } from './dtos/sign-in.dto';
 import { Public } from './public.decorator';
-import { JwtTokensDto } from './dtos/jwt-tokens.dto';
 import { AuthUser } from './user.decorator';
 import { AuthUserDto } from './dtos/auth.user.dto';
 
-@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
@@ -24,19 +21,12 @@ export class AuthController {
   @Public()
   @HttpCode(HttpStatus.OK)
   @Post('login')
-  @ApiOperation({ summary: 'User sign in' })
-  @ApiResponse({ status: 200, description: 'Signed in', type: JwtTokensDto })
-  @ApiResponse({ status: 401, description: 'Invalid credentials' })
-  @ApiResponse({ status: 403, description: 'Email is not verified' })
   signIn(@Body() signInDto: SignInDto) {
     return this.authService.signIn(signInDto.email, signInDto.password);
   }
 
   @Public()
   @Get('refresh')
-  @ApiOperation({ summary: 'Refresh access token' })
-  @ApiResponse({ status: 200, description: 'Token is updated', type: JwtTokensDto })
-  @ApiResponse({ status: 401, description: 'Invalid token' })
   async refreshToken(@Request() req: any) {
     const [type, token] = req.headers.authorization?.split(' ') ?? [];
     if (type !== 'Bearer' || !token) {
