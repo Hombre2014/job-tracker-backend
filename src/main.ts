fa7c60cf98aa6549e6ab4da712690a743a529fd8
Bohdan Shcherbak <b.szczerbak@gmail.com>
2024-10-29 21:32:32 +0100

    Remove swagger ui

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import './utils/array.extensions';
 
@@ -8,16 +7,6 @@ async function bootstrap() {
 
   app.enableCors();
 
-  // Setup Swagger page on /api
-  const config = new DocumentBuilder()
-    .setTitle('Job Tracker API')
-    .setDescription('API description')
-    .setVersion('1.0')
-    .addBearerAuth()
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-
-  SwaggerModule.setup('/api', app, document);
   await app.listen(3000);
 }
 bootstrap();
