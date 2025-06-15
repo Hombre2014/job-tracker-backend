867e963402521960cc23ccefdccefacc05c0c9bb
Bohdan Shcherbak <b.szczerbak@gmail.com>
2025-06-15 20:13:20 +0200

    Attempt to fix CORS

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*',
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:3001',
+      'http://localhost:5173', // Vite default port
+    ],
     credentials: true,
   });
 
