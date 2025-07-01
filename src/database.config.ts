caaea53ea97f253aec72cc9e631ccb5b5e5be76b
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2025-07-01 10:03:45 +0200

    config: add origin and ssl for deployment

--- src/database.config.ts
+++ src/database.config.ts
@@ -1,16 +1,16 @@
-import { DataSourceOptions } from 'typeorm';
 import { config } from 'dotenv';
+import { DataSourceOptions } from 'typeorm';
 import { User } from './modules/users/entities/user.entity';
 import { Board } from './modules/boards/entities/board.entity';
+import { Contact } from './modules/contacts/entities/contact.entity';
+import { Company } from './modules/companies/entities/company.entity';
+import { Document } from './modules/documents/entities/document.entity';
+import { ContactEmail } from './modules/contacts/entities/contact-emails.entity';
+import { ContactPhone } from './modules/contacts/entities/contact-phones.entity';
 import { BoardColumn } from './modules/board-columns/entities/board-column.entity';
 import { JobApplication } from './modules/job-applications/entities/job-application.entity';
 import { UserCodeVerification } from './modules/users/entities/user.code.verification.entity';
-import { Contact } from './modules/contacts/entities/contact.entity';
-import { ContactEmail } from './modules/contacts/entities/contact-emails.entity';
-import { ContactPhone } from './modules/contacts/entities/contact-phones.entity';
 import { JobApplicationNote } from './modules/job-application-notes/entities/job-application-note.entity';
-import { Company } from './modules/companies/entities/company.entity';
-import { Document } from './modules/documents/entities/document.entity';
 
 if (process.env.NODE_ENV !== 'test') {
   config();
@@ -20,29 +20,29 @@ if (process.env.NODE_ENV !== 'test') {
 
 export const getDataSourceOptions = (): DataSourceOptions => ({
   type: 'postgres',
+  synchronize: false,
+  migrationsRun: false,
   host: process.env.DB_HOST,
   port: +process.env.DB_PORT,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
+  logging: ['query', 'warn', 'error'],
+  migrations: ['dist/migrations/**/*.js'],
+  ssl: process.env.NODE_ENV !== 'local' && process.env.NODE_ENV !== 'test', // Enable SSL for all remote environments
   entities: [
     User,
     Board,
+    Contact,
+    Company,
+    Document,
     BoardColumn,
+    ContactEmail,
+    ContactPhone,
     JobApplication,
     JobApplicationNote,
     UserCodeVerification,
-    Contact,
-    ContactEmail,
-    ContactPhone,
-    Company,
-    Document,
   ],
-  migrations: ['dist/migrations/**/*.js'],
-  migrationsRun: false,
-  synchronize: false,
-  logging: ['query', 'warn', 'error'],
-  ssl: process.env.NODE_ENV === 'production', // Only enable SSL in production
 });
 
 export default getDataSourceOptions;
--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,20 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+
 import './utils/array.extensions';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     origin: [
       'http://localhost:3000',
       'http://localhost:3001',
-      'https://job-tracker-git-develop-dans-projects-fd64ea5d.vercel.app',
       'https://online-job-trackr.vercel.app',
-      'http://localhost:5173', // Vite default port
     ],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
-    credentials: true,
   });
 
   await app.listen(3000);
