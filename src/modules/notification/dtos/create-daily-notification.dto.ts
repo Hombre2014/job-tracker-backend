d60f73c0cc09a1a01728b09bc3f4aeedf03627b0
Oleksandr Ollinyk <vov96now@gmail.com>
2025-08-06 13:32:48 +0300

    Add validation decorators to daily and weekly notification DTOs

--- src/modules/notification/dtos/create-daily-notification.dto.ts
+++ src/modules/notification/dtos/create-daily-notification.dto.ts
@@ -1,4 +1,12 @@
+import { IsNumber, IsString, Matches, Max, Min } from 'class-validator';
+
 export class CreateDailyNotification {
+  @IsString()
+  @Matches('^(0[0-9]|1[0-9]|2[\0-3]):[0-5][0-9]$')
   time: string;
+
+  @IsNumber()
+  @Min(-840)
+  @Max(720)
   timezoneOffset: number;
 }
--- src/modules/notification/dtos/create-weekly-notification.dto.ts
+++ src/modules/notification/dtos/create-weekly-notification.dto.ts
@@ -1,6 +1,8 @@
 import { CreateDailyNotification } from './create-daily-notification.dto';
 import { DayOfWeekEnum } from '../enums/day-of-week.enum';
+import { IsEnum } from 'class-validator';
 
 export class CreateWeeklyNotification extends CreateDailyNotification {
+  @IsEnum(DayOfWeekEnum)
   dayOfWeek: DayOfWeekEnum;
 }
