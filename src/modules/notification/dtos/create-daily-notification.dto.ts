e3cdc489a1d5c8cc950563829ccef9018fde57d7
Oleksandr Ollinyk <vov96now@gmail.com>
2025-08-11 15:16:59 +0300

    Fix time regex in DTO

--- src/modules/notification/dtos/create-daily-notification.dto.ts
+++ src/modules/notification/dtos/create-daily-notification.dto.ts
@@ -2,7 +2,7 @@ import { IsNumber, IsString, Matches, Max, Min } from 'class-validator';
 
 export class CreateDailyNotification {
   @IsString()
-  @Matches('^(0[0-9]|1[0-9]|2[\0-3]):[0-5][0-9]$')
+  @Matches('^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$')
   time: string;
 
   @IsNumber()
